5afa6bb5c20574ae71bf96d97352ffecfc286990
494311870 <494311870@qq.com>
2020-10-25 19:22:45 +0800

    创建了Hello_Vue

---
+++ work-05/work/hello_vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <First/>
+        <HelloWorld/>
+
+    </div>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld'
+    import First from "./components/First";
+
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld,
+            First,
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ work-05/work/hello_vue/src/components/First.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        This is My First Components!
+        <p>{{ msg }}</p>
+        <button @click="func">What happen?</button>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "First",
+        data() {
+            return {
+                msg: 'This msg from First.'
+            }
+        },
+        methods:{
+            func(){
+                this.msg = "Nothing happen."
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        color: blueviolet;
+    }
+</style>
